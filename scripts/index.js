671247343ff6b1ef16530f39d575e24d6c417b0b
Anna Gorbacheva <gorbachevaa93@mail.ru>
2025-04-19 22:02:40 +0300

    первая проверка

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
+// Получаем элемент методом querySelector и его содержимое свойством content
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+//Создаём контейнер, в котором хранятся карточки//
+const placesList = document.querySelector(".places__list");
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard({ name, link }, deleteCard) {
+  // Клонируем содержимое тега template
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
 
-// @todo: Функция удаления карточки
+  // Находим и Устанавливаем значения элементов
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__title").textContent = name;
 
-// @todo: Вывести карточки на страницу
+  // Добавляем обработчик клика на кнопку удаления и функцию удаления
+  deleteButton.addEventListener("click", () => {
+    deleteCard(cardElement);
+  });
+  return cardElement;
+}
+
+/// Функция удаления  карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+///Перебираем массив,создаем переменную карточки,вызывая функцию создания карточки и выводим на страницу ///
+
+initialCards.forEach(({ name, link }) => {
+  const card = createCard({ name, link }, deleteCard);
+  placesList.append(card);
+});
